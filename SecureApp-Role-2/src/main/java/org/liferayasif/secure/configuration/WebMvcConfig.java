fb97e457fc3927afc6aa6928d1f8275fca591007
asifaftab87 <asifaftab87@gmail.com>
2019-11-20 00:20:49 +0800

    Now web jars bootstrap and jquery working fine

--- SecureApp-Role-2/src/main/java/org/liferayasif/secure/configuration/WebMvcConfig.java
+++ SecureApp-Role-2/src/main/java/org/liferayasif/secure/configuration/WebMvcConfig.java
@@ -2,15 +2,15 @@ package org.liferayasif.secure.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter{
+public class WebMvcConfig implements WebMvcConfigurer{
 
 	
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-           // registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-            registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+           // registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 }
